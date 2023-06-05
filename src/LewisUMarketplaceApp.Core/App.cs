a42329a6f96c5e86661bbbcd6fa93b2928a7a34b
Elijah Molloy <elijahmolloy@gmail.com>
2023-06-04 19:21:03 -0600

    Initial commit

---
+++ src/LewisUMarketplaceApp.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using LewisUMarketplaceApp.Core.ViewModels.Home;
+
+namespace LewisUMarketplaceApp.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
---
+++ src/LewisUMarketplaceApp.Droid/obj/Debug/android/src/crc64cc81b31a88574f5f/SplashActivity.java
@@ -0,0 +1,48 @@
+package crc64cc81b31a88574f5f;
+
+
+public class SplashActivity
+	extends crc6426126fc9ed8a1cca.MvxFormsSplashScreenActivity_3
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("LewisUMarketplaceApp.Droid.Views.SplashActivity, LewisUMarketplaceApp.Android", SplashActivity.class, __md_methods);
+	}
+
+
+	public SplashActivity ()
+	{
+		super ();
+		if (getClass () == SplashActivity.class) {
+			mono.android.TypeManager.Activate ("LewisUMarketplaceApp.Droid.Views.SplashActivity, LewisUMarketplaceApp.Android", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public SplashActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == SplashActivity.class) {
+			mono.android.TypeManager.Activate ("LewisUMarketplaceApp.Droid.Views.SplashActivity, LewisUMarketplaceApp.Android", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
